aa3abadd1194cd37b7877f1514ac49e527cd886f
hernandez87v <hernandez87v@hotmail.com>
2020-04-28 09:21:29 -0700

    Finished excercise 6/7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create([
+  { first_name: "Khurram", last_name: "Virani", hourly_rate: 60 },
+  { first_name: "John", last_name: "Smith", hourly_rate: 50},
+  { first_name: "Jackie", last_name: "Clark", hourly_rate: 70}
+])
+
+
+@store2.employees.create([
+  { first_name: "Melissa", last_name: "Fox", hourly_rate: 60 },
+  { first_name: "Juan", last_name: "Gonzalez", hourly_rate: 145},
+  { first_name: "Xi", last_name: "Kwon", hourly_rate: 50}
+])
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please add a store name."
+store_name = gets.chomp
+
+insert = Store.create(name: store_name)
+
+puts insert.errors.full_messages
